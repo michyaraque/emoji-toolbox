25f8465bf3031b0294a348a459c33d8f3cbdf458
michyaraque <michyaraque@gmail.com>
2023-01-02 20:53:07 +0100

    test: :test_tube: update test to be used in functional component tests

--- tests/emoji.test.ts
+++ tests/emoji.test.ts
@@ -1,50 +1,47 @@
-import { Emoji } from '../src/index';
 import { describe, beforeEach, it } from "mocha";
 import { expect } from 'chai';
 import { isHexTooDark, isHexTooLight } from '../src/utils';
+import emoji from '../src/core/Emoji';
 
-describe("Show", () => {
+describe("Emoji Toolbox - Package", () => {
 
-    it("Should be instance of Emoji", async () => {
-        const emoji = new Emoji;
-        expect(emoji).to.be.instanceOf(Emoji);
+  describe("~ Emoji data", () => {
+
+    it("Should be the identifier name of the input emoji", async () => {
+      expect(emoji.identifier("☢️")).to.equal("radioactive_sign");
     })
 
-    describe("~ Emoji data", () => {
-
-        let emoji: Emoji;
-        beforeEach(() => {
-            emoji = new Emoji("☢️");
-        })
-
-        it("Should contain emoji data", async () => {
-            expect(emoji.emojiData).to.ok;
-        })
-
-        it("Should be the exact name of the input emoji", async () => {
-            expect(emoji.name()).to.equal("radioactive_sign");
-        })
-
-        it("Darken color of emoji not should be lighten", async() => {
-            expect(isHexTooLight(emoji.darkenColor().hexadecimal)).to.be.false;
-        })
-
-        it("Darken color of emoji not should be darken", async() => {
-            expect(isHexTooDark(emoji.softColor().hexadecimal)).to.be.false;
-        })
-
-        it("Should be the exact unicode of the input emoji", async () => {
-            expect(emoji.unicode()).to.equal("2622-fe0f");
-        })
-
-        it("Should contain exact nested keys", async () => {
-            expect(emoji.normalColor()).to.have.nested.property("hexadecimal");
-            expect(emoji.normalColor()).to.have.nested.property("rgb");
-
-            expect(emoji.normalColor()).to.have.nested.property("rgb.red");
-            expect(emoji.normalColor()).to.have.nested.property("rgb.green");
-            expect(emoji.normalColor()).to.have.nested.property("rgb.blue");
-        })
-
+    it("Should be the exact name of the input emoji", async () => {
+      expect(emoji.humanName("☢️")).to.equal("Radioactive Sign");
     })
+
+    it("Darken color of emoji not should be lighten", async () => {
+      expect(isHexTooLight(emoji.darkenColor("☢️", "hex").toString())).to.be.false;
+    })
+
+    it("Darken color of emoji not should be darken", async () => {
+      expect(isHexTooDark(emoji.softColor("☢️", "hex").toString())).to.be.false;
+    })
+
+    it("Should be the exact unicode of the input emoji", async () => {
+      expect(emoji.unicode("☢️")).to.equal("2622-fe0f");
+    })
+
+  })
+
+  describe("~ Emoji utils", async () => {
+    it("Phrase should contain emoji", async () => {
+      expect(emoji.hasEmoji("🎉 Time to party!")).to.be.true;
+    })
+
+    it("Phrase should not contain emoji", async () => {
+      expect(emoji.hasEmoji("Time to party!")).to.be.false;
+    })
+
+    it("Should remove emoji from string/phrase", async () => {
+      expect(
+        emoji.demojify("Time to party 🎉!")
+      ).to.be.equal("Time to party", "Phrase does not contain emoji or is incorrect");
+    })
+  })
 })
